9256210ed9bcaffcc120e67b162eba93896092d0
Anton Goloborodko <golobor@mit.edu>
2012-03-26 20:40:47 -0400

    Update test_genome.py

--- tests/test_genome.py
+++ tests/test_genome.py
@@ -14,20 +14,41 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_init(self):
         self.assertEqual(
-            self.db.id_to_idx,
+            self.db.str2idx,
             {'1': 0, '2': 1, '3': 2, 'X': 3})
 
         self.assertEqual(
-            self.db.idx_to_id,
+            self.db.idx2str,
             {0: '1', 1: '2', 2: '3', 3: 'X'})
 
         self.assertTrue(numpy.all(numpy.equal(
-            self.db.chromosomes, [49950, 49950, 24950, 49950])))
+            self.db.chrmLens, [49950, 49950, 24950, 49950])))
 
         self.assertTrue(numpy.all(numpy.equal(
-            self.db.centromeres, [3920, 2815, 1500, 6012])))
+            self.db.cntrMids, [3920, 2815, 1500, 6012])))
 
-        print self.db.getRsitesRfrags('HindIII')
+    def test_binning(self):
+        self.db.setResolution(100)
+
+        self.assertTrue(numpy.all(numpy.equal(
+            self.db.chrmLensBin, [500, 500, 250, 500])))
+        self.assertTrue(numpy.all(numpy.equal(
+            self.db.chrmStartsBinCont, [0, 500, 1000, 1250])))
+        self.assertTrue(numpy.all(numpy.equal(
+            self.db.chrmEndsBinCont, [500, 1000, 1250, 1750])))
+        self.assertEqual(
+            self.db.numBins, 1750)
+
+    def test_restriction(self):
+        self.db.setEnzyme('HindIII')
+        self.assertTrue(numpy.all(numpy.equal(
+            self.db.rsites[1],
+            [0, 15300, 16051, 18134, 24994, 28072, 
+             28181, 36365, 39162, 39796, 40431])))
+        self.assertTrue(numpy.all(numpy.equal(
+            self.db.rfragMids[0],
+            [8004, 20291, 26278, 29207, 31293, 32465,
+             35265, 38062, 38582, 39025, 41430, 45030, 48203])))
 
 if __name__ == '__main__':
         unittest.main()
