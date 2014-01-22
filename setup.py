25e662308fe93afd0a83d5448804ecbb8b26736d
Nezar Abdennur <nabdennur@gmail.com>
2014-01-21 20:07:32 -0500

    New global setup.py file handles build and source dist creation with cython optional

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from distutils.extension import Extension
+import numpy
+
+cmdclass = {}
+ext_modules = []
+
+try:
+    from Cython.Distutils import build_ext
+    ext_modules += [
+        Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.pyx" ],
+                  include_dirs=[numpy.get_include()]),
+    ]
+    cmdclass.update( {'build_ext': build_ext} )
+
+except ImportError:
+    ext_modules += [
+        Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.c" ],
+                  include_dirs=[numpy.get_include()]),
+    ]
+
+
+setup(
+    name='mirnylib',
+    url='https://bitbucket.org/mirnylab/mirnylib/',
+    description=('Libraries, shared between different mirnylab projects.'),
+    packages=['mirnylib'],
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
+)
