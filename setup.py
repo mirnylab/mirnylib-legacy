081359bb1a91330408e51b03f7efda01bd0a3a93
Nezar Abdennur <nabdennur@gmail.com>
2014-03-06 04:36:19 -0500

    always use cython when available

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ import numpy
 ext_modules = []
 cmdclass = {}
 
-if os.path.isfile('mirnylib/numutils_new.c'):
+try:
+    from Cython.Distutils import build_ext
+    ext_modules += [
+        Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.pyx" ],
+                  include_dirs=[numpy.get_include()]),
+    ]
+    cmdclass.update( {'build_ext': build_ext} )
+except ImportError:
+    if not os.path.isfile('mirnylib/numutils_new.c'):
+        raise RuntimeError("Cython is required to build extension modules for mirnylib.")
     ext_modules += [
         Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.c" ],
                   include_dirs=[numpy.get_include()]),
     ]
-else:
-    try:
-        from Cython.Distutils import build_ext
-        ext_modules += [
-            Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.pyx" ],
-                      include_dirs=[numpy.get_include()]),
-        ]
-        cmdclass.update( {'build_ext': build_ext} )
-    except ImportError:
-        raise RuntimeError("Cython is required to build extension modules for mirnylib.")
+        
 
 setup(
     name='mirnylib',
