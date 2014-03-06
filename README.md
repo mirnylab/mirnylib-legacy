9b4c47a343c6b99a7e031f3faad21657ae2fb01c
Nezar Abdennur <nabdennur@gmail.com>
2014-03-06 03:55:10 -0500

    Switch to setuptools and declaring install dependencies, add README.md

---
+++ README.md
@@ -0,0 +1,33 @@
+mirnylib
+========
+
+Requirements
+------------
+
+Python 2.6 or 2.7
+
+It is probably a good idea to upgrade to the latest version of setuptools and pip. Follow the PyPA's [guide](http://python-packaging-user-guide.readthedocs.org/en/latest/).
+
+### Non-python dependencies
+
+- For the h5py package to install properly, you need to have the shared library and development headers for HDF5 1.8.4 or newer installed (`libhdf5-dev` or similar). See the [h5py docs](http://docs.h5py.org/en/latest/build.html) for more information.
+
+### Python dependencies
+
+Getting the basic Scientific Python stack (numpy/scipy/matplotlib) can be trickier on some platforms than others. For more details, see the [instructions on scipy.org](http://www.scipy.org/install.html). It is recommended that you already have these dependencies installed and running correctly before attempting to install this package.
+
+These dependencies are automatically installed by setuptools if missing:
+
+- numpy (1.6+)
+- scipy
+- matplotlib
+- biopython
+- joblib (0.6.3+)
+- bx-python (preferably from the [bitbucket repo](https://bitbucket.org/james_taylor/bx-python/wiki/Home) by james_taylor)
+- h5py (see note above)
+
+Recommended but optional:
+
+- Cython (0.16+) to build Cython extensions from .pyx source
+
+
--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 import os
 import numpy
 
-cmdclass = {}
 ext_modules = []
+cmdclass = {}
 
-try:
-    from Cython.Distutils import build_ext
-    ext_modules += [
-        Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.pyx" ],
-                  include_dirs=[numpy.get_include()]),
-    ]
-    cmdclass.update( {'build_ext': build_ext} )
-
-except ImportError:
-    if not os.path.isfile('mirnylib/numutils_new.c'): raise
+if os.path.isfile('mirnylib/numutils_new.c'):
     ext_modules += [
         Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.c" ],
                   include_dirs=[numpy.get_include()]),
     ]
-
+else:
+    try:
+        from Cython.Distutils import build_ext
+        ext_modules += [
+            Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.pyx" ],
+                      include_dirs=[numpy.get_include()]),
+        ]
+        cmdclass.update( {'build_ext': build_ext} )
+    except ImportError:
+        raise RuntimeError("Cython is required to build extension modules for mirnylib.")
 
 setup(
     name='mirnylib',
@@ -29,4 +28,18 @@ setup(
     packages=['mirnylib'],
     ext_modules=ext_modules,
     cmdclass=cmdclass,
+    install_requires=[
+        'cython>=0.16',
+        'numpy>=1.6',
+        'scipy',
+        'matplotlib',
+        'biopython',
+        'pysam',
+        'joblib>=0.6.3',
+        'h5py',
+        'bx-python',
+    ],
+    dependency_links=[
+        'https://bitbucket.org/james_taylor/bx-python/get/tip.tar.bz2'
+    ],
 )
