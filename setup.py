e291130b8deb8a5ce4def350fd46eda1dcb5db1b
Nezar Abdennur <nabdennur@gmail.com>
2016-02-08 01:58:48 -0500

    use clang-omp compiler and its openMP library on Mac by setting CC environment variable

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, Extension
+import sys
 import os
 import numpy
+
 
 ext_modules = []
 cmdclass = {}
@@ -8,28 +10,47 @@ if os.name != "nt":
     try:
         from Cython.Distutils import build_ext
         ext_modules += [
-            Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.pyx" ],
+            Extension(
+                "mirnylib.numutils_new",
+                ["mirnylib/numutils_new.pyx"],
                 language = "c++",
-                      include_dirs=[numpy.get_include()]),
-        ]
-        ext_modules += [
-            Extension("mirnylib.fastExtensions", [ "mirnylib/fastExtensions/fastExtensionspy.pyx", "mirnylib/fastExtensions/fastExtensions.cpp"  ],
-                language = "c++",
-                 include_dirs=[numpy.get_include()], 
-                 extra_compile_args = ["-march=native" , "-Ofast", "-fopenmp"],
-                 extra_link_args = ["-march=native" , "-Ofast", "-lgomp"]),
+                include_dirs=[numpy.get_include()]),
         ]
 
+        # On MacOS, use homebrew-installed LLVM OpenMP if the CC variable is set
+        if sys.platform == 'darwin' and os.environ.get('CC', None) == 'clang-omp':
+            opts = {
+                'language': 'c++',
+                'include_dirs': [numpy.get_include(), '/usr/local/include'],
+                'library_dirs': ['/usr/local/lib'],
+                'extra_compile_args': ["-march=native" , "-Ofast", "-fopenmp"],
+                'extra_link_args': ["-march=native" , "-Ofast", "-liomp5"],
+            }
+        else:
+            opts = {
+                'language': 'c++',
+                'include_dirs': [numpy.get_include()],
+                'extra_compile_args': ["-march=native" , "-Ofast", "-fopenmp"],
+                'extra_link_args': ["-march=native" , "-Ofast", "-lgomp"],
+            }
+
+        ext_modules += [
+            Extension(
+                "mirnylib.fastExtensions",
+                ["mirnylib/fastExtensions/fastExtensionspy.pyx", "mirnylib/fastExtensions/fastExtensions.cpp"],
+                **opts)
+        ]
 
         cmdclass.update( {'build_ext': build_ext} )
-
 
     except ImportError:
         if not os.path.isfile('mirnylib/numutils_new.c'):
             raise RuntimeError("Cython is required to build extension modules for mirnylib.")
         ext_modules += [
-            Extension("mirnylib.numutils_new", [ "mirnylib/numutils_new.c" ],
-                      include_dirs=[numpy.get_include()]),
+            Extension(
+                "mirnylib.numutils_new",
+                ["mirnylib/numutils_new.c"],
+                include_dirs=[numpy.get_include()]),
         ]
 
 
